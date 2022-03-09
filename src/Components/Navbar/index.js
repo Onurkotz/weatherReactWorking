5437de96d4e4f264989cd302b4bfe679c3422c0a
onur koc <onurkottz@gmail.com>
2022-03-09 16:34:25 +0300

    Structure

---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,19 @@
+import styles from "./styles.module.css"
+import * as React from "react";
+
+
+function Navbar() {
+  return (
+    <div className={styles.nav}>
+
+    <ul>
+      <li>
+        <a href> Anasayfa</a>
+      </li>
+    </ul>
+      
+    </div>
+  )
+}
+
+export default Navbar
